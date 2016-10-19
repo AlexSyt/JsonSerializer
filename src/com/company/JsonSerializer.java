51d91906e9cfa562a23cafb81422753e91206c26
Alex <asytnyk0@gmail.com>
2016-10-19 22:59:29 +0300

    the creation of a public interface

---
+++ src/com/company/JsonSerializer.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
+
+public class JsonSerializer {
+    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+    private boolean indent;
+
+    public boolean isIndent() {
+        return indent;
+    }
+
+    public void setIndent(boolean indent) {
+        this.indent = indent;
+    }
+
+    public String serialize(Object obj) {
+        throw new IllegalStateException();
+    }
+
+    public void serialize(Object obj, OutputStream stream) {
+        serialize(obj, stream, DEFAULT_CHARSET);
+    }
+
+    public void serialize(Object obj, OutputStream stream, Charset charset) {
+        serialize(obj, new OutputStreamWriter(stream, charset));
+    }
+
+    public void serialize(Object obj, Writer writer) {
+    }
+}
