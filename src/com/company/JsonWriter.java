5f925c76bbe2e867976b8cc5f81a730bcbf2df67
Alex <asytnyk0@gmail.com>
2016-10-24 23:15:06 +0300

    fixed exception handling

--- src/com/company/JsonWriter.java
+++ src/com/company/JsonWriter.java
@@ -10,47 +10,31 @@ public class JsonWriter {
         this.writer = writer;
     }
 
-    protected void writeObjectBegin() {
-        try {
-            writer.append('{');
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+    protected void writeObjectBegin() throws IOException {
+        writer.append('{');
     }
 
-    protected void writeObjectEnd() {
-        try {
-            writer.append('}');
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+    protected void writeObjectEnd() throws IOException {
+        writer.append('}');
     }
 
-    protected void writeArrayBegin() {
-        try {
-            writer.append('[');
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+    protected void writeArrayBegin() throws IOException {
+        writer.append('[');
     }
 
-    protected void writeArrayEnd() {
-        try {
-            writer.append(']');
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+    protected void writeArrayEnd() throws IOException {
+        writer.append(']');
     }
 
     protected void writeString(String s) {
 
     }
 
-    protected void writeNumber(Number num){
+    protected void writeNumber(Number num) {
 
     }
 
-    protected void writeSeparator(){
+    protected void writeSeparator() {
 
     }
 
