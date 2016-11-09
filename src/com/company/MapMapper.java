44ca669852dee0856cecf115f0a21e0d9ee32197
Alex <asytnyk0@gmail.com>
2016-11-09 21:28:01 +0200

    add MapMapper

---
+++ src/com/company/MapMapper.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MapMapper implements JsonMapper {
+    private JsonSerializer serializer;
+
+    public MapMapper(JsonSerializer serializer) {
+        this.serializer = serializer;
+    }
+
+    @Override
+    public void write(Object obj, JsonWriter writer) {
+        try {
+            if (obj != null) {
+                Map<String, ?> map = (Map<String, ?>) obj;
+                Iterator iter = map.entrySet().iterator();
+                writer.writeObjectBegin();
+                while (iter.hasNext()) {
+                    Map.Entry entry = (Map.Entry) iter.next();
+                    writer.writeString((String) entry.getKey());
+                    writer.writePropertySeparator();
+                    serializer.serialize(entry.getValue(), writer);
+                    if (iter.hasNext()) writer.writeSeparator();
+                }
+                writer.writeObjectEnd();
+            } else writer.writeNull();
+        } catch (IOException e) {
+            // catch exception
+        }
+    }
+}
