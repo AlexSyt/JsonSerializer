6bb469e5ced0db58855eeac039242b47a804c628
Alex <asytnyk0@gmail.com>
2016-11-10 13:33:46 +0200

    delete objects of serializer in BooleanMapper, NumberMapper and StringMapper

--- src/com/company/NumberMapper.java
+++ src/com/company/NumberMapper.java
@@ -3,12 +3,6 @@ package com.company;
 import java.io.IOException;
 
 public class NumberMapper implements JsonMapper {
-    private JsonSerializer serializer;
-
-    public NumberMapper(JsonSerializer serializer) {
-        this.serializer = serializer;
-    }
-
     @Override
     public void write(Object obj, JsonWriter writer) {
         try {
--- src/com/company/StringMapper.java
+++ src/com/company/StringMapper.java
@@ -3,12 +3,6 @@ package com.company;
 import java.io.IOException;
 
 public class StringMapper implements JsonMapper {
-    private JsonSerializer serializer;
-
-    public StringMapper(JsonSerializer serializer) {
-        this.serializer = serializer;
-    }
-
     @Override
     public void write(Object obj, JsonWriter writer) {
         try {
