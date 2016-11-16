a674c7756587953723610151df0aed9dd441f875
Alex <asytnyk0@gmail.com>
2016-11-16 20:50:45 +0200

    add functionality to PrimitiveArrayMapper

--- src/com/company/PrimitiveArrayMapper.java
+++ src/com/company/PrimitiveArrayMapper.java
@@ -1,12 +1,25 @@
 package com.company;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 public class PrimitiveArrayMapper implements JsonMapper {
+    private JsonSerializer serializer;
+
+    public PrimitiveArrayMapper(JsonSerializer serializer) {
+        this.serializer = serializer;
+    }
+
     @Override
     public void write(Object obj, JsonWriter writer) throws IOException {
         if (obj != null) {
-
+            int size = Array.getLength(obj);
+            writer.writeArrayBegin();
+            for (int i = 0; i < size; i++) {
+                serializer.serialize(Array.get(obj, i), writer);
+                if (i != size - 1) writer.writeSeparator();
+            }
+            writer.writeArrayEnd();
         } else writer.writeNull();
     }
 }
