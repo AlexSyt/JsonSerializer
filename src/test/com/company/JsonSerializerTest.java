71c95cb88380377ce08399454fc1c72b4a374280
Alex <asytnyk0@gmail.com>
2016-11-25 01:27:00 +0200

    fix argument order in assertEquals

--- src/test/com/company/JsonSerializerTest.java
+++ src/test/com/company/JsonSerializerTest.java
@@ -11,12 +11,12 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class JsonSerializerTest {
     @Test
-    public void serialize() throws Exception {
+    public void testSerialize() throws Exception {
         JsonSerializer ser = new JsonSerializer();
         Integer[] arr = {1, 2, 3, 4, 5};
         ArrayList<Integer> toSer = new ArrayList<>(Arrays.asList(arr));
         StringWriter sw = new StringWriter();
         ser.serialize(toSer, sw);
-        assertEquals(sw.toString(), "[1,2,3,4,5]");
+        assertEquals("[1,2,3,4,5]", sw.toString());
     }
 }
