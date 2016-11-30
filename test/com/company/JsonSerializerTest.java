071ec9a423d4a26014bed0f840cb48fc909a2627
Alex <asytnyk0@gmail.com>
2016-11-30 23:18:46 +0200

    add test

--- test/com/company/JsonSerializerTest.java
+++ test/com/company/JsonSerializerTest.java
@@ -3,15 +3,16 @@ package com.company;
 import com.google.gson.Gson;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class JsonSerializerTest {
     @Test
     public void serialize() throws Exception {
+        JsonSerializer ser = new JsonSerializer();
+        Gson gson = new Gson();
+
         Integer[] arrInt1 = {1, 2, 3, 4, 5};
         Integer[] arrInt2 = {11, 22, 33, 44, 55};
         Integer[] arrInt3 = {111, 222, 333, 444, 555};
@@ -30,12 +31,33 @@ public class JsonSerializerTest {
         list2.add(arrInt12);
         list2.add(arrInt13);
 
-        Map<String, ArrayList<Integer[]>> map = new HashMap<>();
-        map.put("list1", list1);
-        map.put("list2", list2);
+        Map<String, ArrayList<Integer[]>> mapTest = new HashMap<>();
+        mapTest.put("list1", list1);
+        mapTest.put("list2", list2);
 
-        JsonSerializer ser = new JsonSerializer();
-        Gson gson = new Gson();
-        assertEquals(gson.toJson(map), ser.serialize(map));
+
+
+        List<Map<String, LinkedList<Double[]>>> listTest = new ArrayList<>();
+
+        Double[] arrD1 = {1.5, 2.5, 3.5};
+        Double[] arrD2 = {2.5, 3.5, 4.5};
+        Double[] arrD11 = {3.5, 4.5, 5.5};
+        Double[] arrD22 = {4.5, 5.5, 6.5};
+
+        LinkedList<Double[]> l1 = new LinkedList<>();
+        LinkedList<Double[]> l2 = new LinkedList<>();
+        l1.add(arrD1);
+        l1.add(arrD2);
+        l2.add(arrD11);
+        l2.add(arrD22);
+
+        Map<String, LinkedList<Double[]>> m = new TreeMap<>();
+        m.put("item1", l1);
+        m.put("item2", l2);
+
+        listTest.add(m);
+
+        assertEquals(gson.toJson(mapTest), ser.serialize(mapTest));
+        assertEquals(gson.toJson(listTest), ser.serialize(listTest));
     }
 }
