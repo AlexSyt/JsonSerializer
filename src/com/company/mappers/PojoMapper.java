0d8f80e09a74e73d891c253ed7699aad73573d08
Alex <asytnyk0@gmail.com>
2016-12-01 23:55:10 +0200

    create PojoMapper

---
+++ src/com/company/mappers/PojoMapper.java
@@ -0,0 +1,12 @@
+package com.company.mappers;
+
+import com.company.JsonWriter;
+
+import java.io.IOException;
+
+public class PojoMapper implements JsonMapper {
+    @Override
+    public void write(Object obj, JsonWriter writer) throws IOException {
+
+    }
+}
