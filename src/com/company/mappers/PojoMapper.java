8443406467924aa13fed417d79fa64f00e6b4ce8
Alex <asytnyk0@gmail.com>
2016-12-04 22:25:57 +0200

    modify PojoMapper

--- src/com/company/mappers/PojoMapper.java
+++ src/com/company/mappers/PojoMapper.java
@@ -1,12 +1,37 @@
 package com.company.mappers;
 
+import com.company.JsonSerializer;
 import com.company.JsonWriter;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 public class PojoMapper implements JsonMapper {
+    private JsonSerializer serializer;
+    private Field[] fields;
+
+    public PojoMapper(JsonSerializer serializer, Object object) {
+        this.serializer = serializer;
+        fields = object.getClass().getDeclaredFields();
+    }
+
     @Override
     public void write(Object obj, JsonWriter writer) throws IOException {
-
+        if (obj != null) {
+            writer.writeObjectBegin();
+            for (int i = 0; i < fields.length; i++) {
+                Field f = fields[i];
+                f.setAccessible(true);
+                writer.writeString(f.getName());
+                writer.writePropertySeparator();
+                try {
+                    serializer.serialize(f.get(obj));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                if (i != fields.length - 1) writer.writeSeparator();
+            }
+            writer.writeObjectEnd();
+        } else writer.writeNull();
     }
 }
