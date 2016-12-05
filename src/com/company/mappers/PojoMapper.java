be849416d332e776b8cb8c0077810d2cdb0860f9
Alex <asytnyk0@gmail.com>
2016-12-05 22:50:49 +0200

    modify PojoMapper

--- src/com/company/mappers/PojoMapper.java
+++ src/com/company/mappers/PojoMapper.java
@@ -5,22 +5,28 @@ import com.company.JsonWriter;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class PojoMapper implements JsonMapper {
     private JsonSerializer serializer;
-    private final Field[] fields;
+    private final ArrayList<Field> fields;
 
     public PojoMapper(JsonSerializer serializer, Class clazz) {
         this.serializer = serializer;
-        fields = clazz.getDeclaredFields();
+        ArrayList<Field> f = new ArrayList<>();
+        for (Class cur = clazz; cur != Object.class; cur = cur.getSuperclass()) {
+            for (Field field : cur.getDeclaredFields())
+                f.add(field);
+        }
+        fields = f;
     }
 
     @Override
     public void write(Object obj, JsonWriter writer) throws IOException {
         if (obj != null) {
             writer.writeObjectBegin();
-            for (int i = 0; i < fields.length; i++) {
-                Field f = fields[i];
+            for (int i = 0; i < fields.size(); i++) {
+                Field f = fields.get(i);
                 f.setAccessible(true);
                 writer.writeString(f.getName());
                 writer.writePropertySeparator();
@@ -29,7 +35,7 @@ public class PojoMapper implements JsonMapper {
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
-                if (i != fields.length - 1) writer.writeSeparator();
+                if (i != fields.size() - 1) writer.writeSeparator();
             }
             writer.writeObjectEnd();
         } else writer.writeNull();
